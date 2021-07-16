e46a125121ac199b3a4c1e211f139777f20e4e2f
Furkan <Furkan@DESKTOP-IT8CT14>
2021-07-16 20:10:40 +0300

    Proje dosyası ekle.

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Id: " + musteri.Id + " - Adı: " + musteri.Ad + " Soyadı: " + musteri.Soyad);
+            Console.WriteLine("Müşteri Eklenmiştir.");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad);
+        }
+        
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silinmiştir. Silinen müşteri: \n" + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0;
+            musteri1.Ad = "Furkan";
+            musteri1.Soyad = "KORKMAZGÖZ";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1;
+            musteri2.Ad = "Ahmet";
+            musteri2.Soyad = "GÜNDOĞDU";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 2;
+            musteri3.Ad = "Mehmet";
+            musteri3.Soyad = "ÇAKIR";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri: \n Id: "+musteri.Id + " - Adı: " + musteri.Ad + " Soyadı: " + musteri.Soyad);
+            }
+
+            Console.WriteLine("\n------Döngü Sonu------\n");
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+
+            Console.WriteLine("\nMüşteri Listesi: \n");
+
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+
+            Console.WriteLine("\n");
+            musteriManager.Sil(musteri1);
+
+        }
+    }
+}
